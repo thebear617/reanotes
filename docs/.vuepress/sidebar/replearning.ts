cd73512a47c3d89814d9a8d5667ed9d01c2a072d
莫开车 <1357953389@qq.com>
2026-07-17 17:36:07 +0800

    feat: 建立表征学习双轴框架

--- docs/.vuepress/sidebar/replearning.ts
+++ docs/.vuepress/sidebar/replearning.ts
@@ -3,4 +3,11 @@ import { arraySidebar } from "vuepress-theme-hope";
 export default arraySidebar([
   "",
   { text: "基础知识", icon: "seedling", link: "foundation/" },
+  { text: "双轴框架", icon: "crosshairs", link: "framework/" },
+  { text: "方法坐标", icon: "table", link: "method-map/" },
+  {
+    text: "实现机制与方法谱系",
+    icon: "gears",
+    link: "mechanisms/",
+  },
 ]);
