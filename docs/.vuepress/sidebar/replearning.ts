3417f936745d8bb463c300399b840da7a2414143
莫开车 <1357953389@qq.com>
2026-07-18 18:59:01 +0800

    refactor: 合并表征学习双轴内容

--- docs/.vuepress/sidebar/replearning.ts
+++ docs/.vuepress/sidebar/replearning.ts
@@ -3,6 +3,4 @@ import { arraySidebar } from "vuepress-theme-hope";
 export default arraySidebar([
   "",
   { text: "基础知识", icon: "seedling", link: "foundation/" },
-  { text: "双轴框架", icon: "crosshairs", link: "framework/" },
-  { text: "方法坐标", icon: "table", link: "method-map/" },
 ]);
