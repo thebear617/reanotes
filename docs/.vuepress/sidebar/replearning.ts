ca6e45e4ffd572aba76ace81ceed163b3e163ec6
莫开车 <1357953389@qq.com>
2026-07-18 18:41:25 +0800

    refactor: 收紧表征学习双轴目录

--- docs/.vuepress/sidebar/replearning.ts
+++ docs/.vuepress/sidebar/replearning.ts
@@ -5,9 +5,4 @@ export default arraySidebar([
   { text: "基础知识", icon: "seedling", link: "foundation/" },
   { text: "双轴框架", icon: "crosshairs", link: "framework/" },
   { text: "方法坐标", icon: "table", link: "method-map/" },
-  {
-    text: "实现机制与方法谱系",
-    icon: "gears",
-    link: "mechanisms/",
-  },
 ]);
